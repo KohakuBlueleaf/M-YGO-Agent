90bf0907b3fc9bcd140aa6862b2cfdcea6ff75cf
Kohaku-Blueleaf <59680068+KohakuBlueleaf@users.noreply.github.com>
2025-07-30 11:49:47 +0800

    Update xmake config for moved src dir

--- xmake.lua
+++ xmake.lua
@@ -3,14 +3,14 @@ add_rules("mode.debug", "mode.release")
 add_repositories("my-repo repo")
 
 add_requires(
-    "ygopro-core 0.0.2", "edopro-core", "pybind11 2.13.*", "fmt 10.2.*", "glog 0.6.0",
+    "ygopro-core 0.0.2", "edopro-core 0.0.2", "pybind11 2.13.*", "fmt 10.2.*", "glog 0.6.0",
     "sqlite3 3.43.0+200", "concurrentqueue 1.0.4", "unordered_dense 4.4.*",
     "sqlitecpp 3.2.1")
 
 
 target("ygopro0_ygoenv")
     add_rules("python.library")
-    add_files("ygoenv/ygoenv/ygopro0/*.cpp")
+    add_files("src/ygoenv/ygopro0/*.cpp")
     add_packages("pybind11", "fmt", "glog", "concurrentqueue", "sqlitecpp", "unordered_dense", "ygopro-core")
     set_languages("c++17")
     if is_mode("release") then
@@ -18,9 +18,10 @@ target("ygopro0_ygoenv")
         add_cxxflags("-march=native")
     end
     add_includedirs("ygoenv")
+    add_ldflags("-Wl,--no-undefined") -- From linkoptions for linux
 
     after_build(function (target)
-        local install_target = "$(projectdir)/ygoenv/ygoenv/ygopro0"
+        local install_target = "$(projectdir)/src/ygoenv/ygopro0"
         os.cp(target:targetfile(), install_target)
         print("Copy target to " .. install_target)
     end)
@@ -28,7 +29,7 @@ target("ygopro0_ygoenv")
 
 target("ygopro_ygoenv")
     add_rules("python.library")
-    add_files("ygoenv/ygoenv/ygopro/*.cpp")
+    add_files("src/ygoenv/ygopro/*.cpp")
     add_packages("pybind11", "fmt", "glog", "concurrentqueue", "sqlitecpp", "unordered_dense", "ygopro-core")
     set_languages("c++17")
     if is_mode("release") then
@@ -36,16 +37,17 @@ target("ygopro_ygoenv")
         add_cxxflags("-march=native")
     end
     add_includedirs("ygoenv")
+    add_ldflags("-Wl,--no-undefined") -- From linkoptions for linux
 
     after_build(function (target)
-        local install_target = "$(projectdir)/ygoenv/ygoenv/ygopro"
+        local install_target = "$(projectdir)/src/ygoenv/ygopro"
         os.cp(target:targetfile(), install_target)
         print("Copy target to " .. install_target)
     end)
 
 target("edopro_ygoenv")
     add_rules("python.library")
-    add_files("ygoenv/ygoenv/edopro/*.cpp")
+    add_files("src/ygoenv/edopro/*.cpp")
     add_packages("pybind11", "fmt", "glog", "concurrentqueue", "sqlitecpp", "unordered_dense", "edopro-core")
     set_languages("c++17")
     if is_mode("release") then
@@ -53,28 +55,10 @@ target("edopro_ygoenv")
         add_cxxflags("-march=native")
     end
     add_includedirs("ygoenv")
+    add_ldflags("-Wl,--no-undefined") -- From linkoptions for linux
 
     after_build(function (target)
-        local install_target = "$(projectdir)/ygoenv/ygoenv/edopro"
+        local install_target = "$(projectdir)/src/ygoenv/edopro"
         os.cp(target:targetfile(), install_target)
         print("Copy target to " .. install_target)
-    end)
-
-
-target("alphazero_mcts")
-    add_rules("python.library")
-    add_files("mcts/mcts/alphazero/*.cpp")
-    add_packages("pybind11")
-    set_languages("c++17")
-    if is_mode("release") then
-        set_policy("build.optimization.lto", true)
-        add_cxxflags("-march=native")
-    end
-    add_includedirs("mcts")
-
-    after_build(function (target)
-        local install_target = "$(projectdir)/mcts/mcts/alphazero"
-        os.cp(target:targetfile(), install_target)
-        print("Copy target to " .. install_target)
-        os.run("pybind11-stubgen mcts.alphazero.alphazero_mcts -o %s", "$(projectdir)/mcts")
     end)
